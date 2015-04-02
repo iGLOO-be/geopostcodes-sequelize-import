8aa1be418d8c5bf1d60d626506447d814501e3ec
Andy Vanbutsele <andy.vanbutsele@gmail.com>
2015-04-02 15:08:36 +0200

    Initialize repository

---
+++ README.md
@@ -0,0 +1,19 @@
+# geoposition-import
+
+
+
+[![build status](https://secure.travis-ci.org/Natsuke/geoposition-import.png)](http://travis-ci.org/Natsuke/geoposition-import)
+
+## Installation
+
+This module is installed via npm:
+
+``` bash
+$ npm install geoposition-import
+```
+
+## Example Usage
+
+``` js
+var geopositionImport = require('geoposition-import');
+```
---
+++ test/import.js
@@ -0,0 +1,44 @@
+var Sequelize = require('sequelize');
+var path = require('path');
+var chai = require('chai');
+var expect = chai.expect;
+
+var testConfig = {
+  database: 'geopostcodes-sequelize-import',
+  user: 'root',
+  password: ''
+};
+
+describe('Import Factory', function () {
+  var sequelize;
+
+  before(function () {
+    sequelize = new Sequelize(
+      testConfig.database,
+      testConfig.user,
+      testConfig.password
+    );
+
+    sequelize.import(path.join(__dirname, '../lib/model'));
+  });
+
+  beforeEach(function (done) {
+    sequelize
+      .sync({ force: true })
+      .nodeify(done);
+  });
+
+  it('Can create importer', function () {
+    var factory = require('../index');
+    importer = factory(sequelize.model('Address'), {});
+
+    expect(importer.syncStream).to.be.a('function');
+  });
+
+  it('Can do import', function (done) {
+    var factory = require('../index');
+    var importer = factory(sequelize.model('Address'), {});
+
+    importer.syncStream({}, done);
+  });
+});
